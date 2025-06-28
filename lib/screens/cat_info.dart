b534eb519b7987adccff7ded51f4f7dc335b3e4f
razksr <felixzukerberg@gmail.com>
2025-06-29 01:07:06 +0700

    commit

---
+++ lib/screens/cat_info.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import '../api/cat_api.dart';
+import '../models/cats.dart';
+
+class CatInfo extends StatefulWidget {
+  final String catBreed;
+  final String catId;
+
+  CatInfo({required this.catBreed, required this.catId});
+
+  @override
+  State<CatInfo> createState() => _CatInfoState();
+}
+
+class _CatInfoState extends State<CatInfo> {
+  CatList catList = CatList(breeds: List.empty());
+
+  void getCatData() async {
+    final catJson = await CatAPI().getCatBreed(widget.catId);
+
+    final dynamic catMap = json.decode(catJson);
+
+    setState(() {
+      catList = CatList.fromJson(catMap);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCatData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.catBreed),
+        ),
+        body: getCat());
+  }
+
+  Widget getCat() {
+    final mediaSize = MediaQuery.of(context).size;
+    if (catList.breeds.isEmpty) {
+      return Container();
+    } else {
+      return Center(
+        child: Container(
+          width: mediaSize.width,
+          height: mediaSize.height,
+          // 1
+           decoration: BoxDecoration(
+           image: DecorationImage(
+           // 2
+          image: NetworkImage(catList.breeds[0].url), fit: BoxFit.contain,
+        )),
+        ),
+      );
+    }
+  }
+}
